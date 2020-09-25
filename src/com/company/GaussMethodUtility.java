def42b629ebfd882642a2543dc7503be2b2ff746
Кирилл Курко <kirillkurko2609@gmail.com>
2020-09-25 07:45:13 +0300

    calculateAnswer implemented

--- src/com/company/GaussMethodUtility.java
+++ src/com/company/GaussMethodUtility.java
@@ -2,9 +2,20 @@ package com.company;
 
 public class GaussMethodUtility {
 
-    private static void forwardMovement(double[][] a, double[] x) {
-        int rowsAmount = a.length;
-        int columnsAmount = a[0].length;
+    private double[][] a;
+    private double[] x;
+    private int rowsAmount;
+    private int columnsAmount;
+
+
+    public  GaussMethodUtility(double[][] a, double[] x) {
+        this.a = a;
+        this.x = x;
+        rowsAmount = a.length;
+        columnsAmount = a[0].length;
+    }
+
+    private void forwardMovement() {
         for (int j = 0; j < rowsAmount - 1; ++j) {
             for (int i = j + 1; i < rowsAmount; ++i) {
                 double temp = a[i][j];
@@ -16,9 +27,7 @@ public class GaussMethodUtility {
         }
     }
 
-    private static void reverseMovement(double[][] a, double[] x) {
-        int rowsAmount = a.length;
-        int columnsAmount = a[0].length;
+    private void reverseMovement(double[][] a, double[] x) {
         for (int j = rowsAmount - 1; j > 0; --j) {
             for (int i = j - 1; i >= 0; --i) {
                 double temp = a[i][j];
@@ -30,5 +39,10 @@ public class GaussMethodUtility {
         }
     }
 
-    private static void
+    private void calculateAnswer(double [][] a, double[] x) {
+        for (int i = 0; i < rowsAmount; ++i) {
+            x[i] /= a[i][i];
+            a[i][i] /= a[i][i];
+        }
+    }
 }
