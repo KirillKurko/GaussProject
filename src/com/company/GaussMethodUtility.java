8af85eaec9479ec7cb91479d84460fab21ef8210
Кирилл Курко <kirillkurko2609@gmail.com>
2020-09-25 08:38:48 +0300

    All is implemented

--- src/com/company/GaussMethodUtility.java
+++ src/com/company/GaussMethodUtility.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class GaussMethodUtility {
 
     private double[][] a;
@@ -8,56 +11,58 @@ public class GaussMethodUtility {
     private int columnsAmount;
 
 
-    public  GaussMethodUtility(double[][] a, double[] x) {
-        this.a = a;
-        this.x = x;
+    public GaussMethodUtility(double[][] a, double[] x) {
         rowsAmount = a.length;
         columnsAmount = a[0].length;
+        this.a = new double[rowsAmount][];
+        for (int i = 0; i < rowsAmount; ++i) {
+            this.a[i] = a[i].clone();
+        }
+        this.x = x.clone();
     }
 
-    public void gaussMethod() {
+    public double[] gaussMethod() {
         forwardMovement();
         reverseMovement();
         calculateAnswer();
+        return x;
     }
 
     private void forwardMovement() {
-        for (int j = 0; j < rowsAmount - 1; ++j) {
-            for (int i = j + 1; i < rowsAmount; ++i) {
+        for (int j = 0; j < rowsAmount - 1; j++) {
+            for (int i = j + 1; i < rowsAmount; i++) {
                 double temp = a[i][j];
-                for (int k = 0; k < columnsAmount; ++k) {
+                for (int k = 0; k < columnsAmount; k++)
                     a[i][k] -= a[j][k] * temp / a[j][j];
-                    x[i] -= x[j] * temp / a[j][j];
-                }
+                x[i] -= x[j] * temp / a[j][j];
             }
         }
     }
 
     private void reverseMovement() {
-        for (int j = rowsAmount - 1; j > 0; --j) {
-            for (int i = j - 1; i >= 0; --i) {
+        for (int j = rowsAmount - 1; j > 0; j--) {
+            for (int i = j - 1; i > -1; i--) {
                 double temp = a[i][j];
-                for (int k = columnsAmount - 1; k >= 0; --k) {
+                for (int k = columnsAmount - 1; k > -1; k--)
                     a[i][k] -= a[j][k] * temp / a[j][j];
-                    x[i] -= x[j] * temp / a[j][j];
-                }
+                x[i] -= x[j] * temp / a[j][j];
             }
         }
     }
 
     private void calculateAnswer() {
-        for (int i = 0; i < rowsAmount; ++i) {
+        for(int i = 0; i < rowsAmount; i++) {
             x[i] /= a[i][i];
             a[i][i] /= a[i][i];
         }
     }
 
-    public double calculateDiscrepancy(double[] b) {
+    public double calculateDiscrepancy(double[][] a, double[] b, double[] x) {
         double currentDiscrepancy = 0;
         double maxDiscrepancy = 0;
-        for (int i = 0; i < rowsAmount; ++i) {
+        for (int i = 0; i < rowsAmount; i++) {
             double q = 0;
-            for (int j = 0; j < columnsAmount; ++j) {
+            for (int j = 0; j < columnsAmount; j++) {
                 q += a[i][j] * x[j];
             }
             currentDiscrepancy = Math.abs(b[i] - q);
@@ -67,5 +72,4 @@ public class GaussMethodUtility {
         }
         return maxDiscrepancy;
     }
-
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        double[][] a = new double[][] {{-2.86, 1, 0, 0, 0},
+                                        {1, -2, 1, 0, 0},
+                                        {0, 1, -2, 1, 0},
+                                        {0, 0, 1, -2, 1},
+                                        {0, 0, 0, 1, -2.86}};
+        double[] x = new double[] {0, -7, -7, -7, 0};
+        double[] b = new double[] {0, -7, -7, -7, 0};
+
+        GaussMethodUtility gaussMethodUtility = new GaussMethodUtility(a, x);
+
+        double[] answer = gaussMethodUtility.gaussMethod();
+        double discrepancy = gaussMethodUtility.calculateDiscrepancy(a, b, answer);
+
+        System.out.println("Answer: " + Arrays.toString(answer));
+        System.out.println("Discrepancy: " + discrepancy);
     }
 }
